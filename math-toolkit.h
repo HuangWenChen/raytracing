c8b41487b4cc72fd32dccdbc1440526d69526509
HuangWenChen <cbt0009212@gmail.com>
2016-09-29 09:59:51 +0800

    subtract_vector use loop unrolling

--- math-toolkit.h
+++ math-toolkit.h
@@ -32,8 +32,9 @@ void add_vector(const double *a, const double *b, double *out)
 static inline
 void subtract_vector(const double *a, const double *b, double *out)
 {
-    for (int i = 0; i < 3; i++)
-        out[i] = a[i] - b[i];
+    out[0] = a[0] - b[0];
+    out[1] = a[1] - b[1];
+    out[2] = a[2] - b[2];
 }
 
 static inline
